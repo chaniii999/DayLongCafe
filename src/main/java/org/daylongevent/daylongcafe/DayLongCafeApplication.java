2b4fa80196d15368b6975db619cfcd0686130b9a
chan <psc0729@naver.com>
2025-02-17 20:47:33 +0900

    Initial commit

---
+++ src/main/java/org/daylongevent/daylongcafe/DayLongCafeApplication.java
@@ -0,0 +1,13 @@
+package org.daylongevent.daylongcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayLongCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayLongCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/daylongevent/daylongcafe/DayLongCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daylongevent.daylongcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayLongCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
