d8566e055f5cd03b4110ec58c674139430608684
chan <psc0729@naver.com>
2025-02-21 20:17:36 +0900

    User 엔티티 필요 잔 수 멤버 추가

--- src/main/java/org/daylongevent/daylongcafe/entity/User.java
+++ src/main/java/org/daylongevent/daylongcafe/entity/User.java
@@ -13,5 +13,11 @@ public class User {
     private String backNumber;
     private int cupsPurchased;
     private int rank;
+    private int requiredCupsNextRank; // 다음 리워드 순위에 들기위한 필요 잔 수
 }
 
+// # 20위 이상인 경우
+// " 010-****-1234 님의 랭킹은 25위입니다. 소비잔 수는 3잔 입니다. 20위까지 n잔(20위의 잔 수 - 내 소비잔 수 + 1) 남았습니다.
+
+// # 20위 이하인 경우
+// " 010-****-1234 님의 랭킹은 18위입니다. 소비잔 수는 10잔 입니다. 5위까지 n잔(5위의 잔 수 - 내 소비잔 수 + 1) 남았습니다.
