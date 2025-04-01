c7da77cdfdbd7ef79875c261efc9bb7d73706eb2
chaniii999 <psc0729@naver.com>
2025-04-01 17:13:58 +0900

    불필요 api 제거

--- src/main/java/org/daylongevent/daylongcafe/controller/SheetController.java
+++ src/main/java/org/daylongevent/daylongcafe/controller/SheetController.java
@@ -23,33 +23,33 @@ public class SheetController {
     @Autowired
     private GoogleSheetsService sheetService;
 
-    @GetMapping("/")
-    public ResponseEntity<?> home() throws GeneralSecurityException, IOException {
-        List<User> allUsersByRank = sheetService.getAllUsersByRank();
+//    @GetMapping("/")
+//    public ResponseEntity<?> home() throws GeneralSecurityException, IOException {
+//        List<User> allUsersByRank = sheetService.getAllUsersByRank();
+//
+//        return ResponseEntity.ok(allUsersByRank);
+//    }
 
-        return ResponseEntity.ok(allUsersByRank);
-    }
-
-    @GetMapping("/search/{phoneNumber}/message")
-    public ResponseEntity<?> searchMessage(@PathVariable String phoneNumber) throws GeneralSecurityException, IOException {
-        User user = sheetService.searchByPhoneNumber(phoneNumber);
-
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("사용자를 찾을 수 없습니다.");
-        }
-
-        String displayMessage =
-            "010-****-" + user.getBackNumber() + " 님의 소비 잔 수는 " + user.getCups() + "이고 "
-                + user.getRank() + "등입니다." +
-                "다음 랭킹까지" + user.getRequiredCupsNextRank() + "잔 남았습니다.\n" +
-                    "성별" + user.getSex() + "생일" + user.getBirthDate() + "입니다.";
-
-        if (user.getRank() == 1) {
-            displayMessage = "010-****-" + user.getBackNumber() + " 님의 소비 잔 수는 " + user.getCups() + "이고 "+ user.getRank() + "등입니다.\n" +
-            "성별" + user.getSex() + "생일" + user.getBirthDate() + "입니다.";
-        }
-        return ResponseEntity.ok(displayMessage);
-    }
+//    @GetMapping("/search/{phoneNumber}/message")
+//    public ResponseEntity<?> searchMessage(@PathVariable String phoneNumber) throws GeneralSecurityException, IOException {
+//        User user = sheetService.searchByPhoneNumber(phoneNumber);
+//
+//        if (user == null) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("사용자를 찾을 수 없습니다.");
+//        }
+//
+//        String displayMessage =
+//            "010-****-" + user.getBackNumber() + " 님의 소비 잔 수는 " + user.getCups() + "이고 "
+//                + user.getRank() + "등입니다." +
+//                "다음 랭킹까지" + user.getRequiredCupsNextRank() + "잔 남았습니다.\n" +
+//                    "성별" + user.getSex() + "생일" + user.getBirthDate() + "입니다.";
+//
+//        if (user.getRank() == 1) {
+//            displayMessage = "010-****-" + user.getBackNumber() + " 님의 소비 잔 수는 " + user.getCups() + "이고 "+ user.getRank() + "등입니다.\n" +
+//            "성별" + user.getSex() + "생일" + user.getBirthDate() + "입니다.";
+//        }
+//        return ResponseEntity.ok(displayMessage);
+//    }
 
     @GetMapping("/search/{phoneNumber}")
     public ResponseEntity<?> search(@PathVariable String phoneNumber) throws GeneralSecurityException, IOException {
